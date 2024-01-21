e4668cb86055c3e7667e376df662746b107aa989
TheMiningD <42899914+TheMiningD@users.noreply.github.com>
2024-01-21 19:05:17 +1000

    Update server.js

--- server.js
+++ server.js
@@ -197,5 +197,5 @@ res.status(500).send('Error calculating average serve time');
 });
 app.use(express.static('public'));
 app.listen(port, () => {
-console.log(Server running at http://localhost:${port});
+console.log(`Server running at http://localhost:${port}`);
 });
